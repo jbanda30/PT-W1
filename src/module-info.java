56fbf56a1293bafccea1b7228677493c4d426112
jbanda30 <113069730+jbanda30@users.noreply.github.com>
2022-09-09 15:28:42 -0500

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,16 @@
-module Week1Homework {
+
+public class Main {
+	
+	public static void main(String[] args) {
+		   // TODO Auto-generated method stub
+		
+		   // create double variables and assign values
+		   double value1 = 14.99;
+		   double value2 = 15.99;
+		   
+		   // add the two values together and print to the console
+		   System.out.print1n(value1 + value2);
+		   
+	}
+
 }
